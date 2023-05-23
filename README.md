a69023c05412009106a13abd48e68588a3f48f81
Mwangi-dan <d.ndungu@alustudent.com>
2023-05-23 23:15:22 +0300

    finally

---
+++ README.md
@@ -0,0 +1,10 @@
+# Regex Hackathon
+This is the repo containing code for the regex hackathon.
+
+## Summary
+The 'javascript_code' folder contains the code for the website. It contains a HTML and javascript file.
+- The output runs on a web browser.
+- It contains a javavscript and html file
+
+The 'python_code' folder contains the implementation of the hackathon in python.
+- The output runs on a terminal.
--- javascript_code/script.js
+++ javascript_code/script.js
@@ -20,63 +20,138 @@ function validateInput() {
 
             document.getElementById("example").innerHTML = "Hint: Title (yyyy)";
             pattern = /^(.+) \(\d{4}\)$/;
-            errorMessage = "Movie titles can only contain letters, digits, spaces, and the characters: '.,?!-";
+            errorMessage = userInput + " is not in the correct format. Check Hint!";
             break;
         case "2": // Song Lyrics
 
             document.getElementById("example").innerHTML = "Hint: [Verse X] some lyrics";
             pattern = /^\[Verse \d+\] (.+)/;
-            errorMessage = "Song lyrics can only contain letters, digits, spaces, and the characters: '.,?!-";
+            errorMessage = userInput + " is not in the correct format. Check Hint!";
             break;
 
         case "3": // Twitter Username
             document.getElementById("example").innerHTML = "Hint: @username";
             pattern = /^@\w+/;
-            errorMessage = "Twitter usernames must be 1-15 characters long and can only contain letters, digits, and underscores.";
+            errorMessage = userInput + " is not in the correct format. Check Hint!";
             break;
 
         case "4": // ISBN Number
             document.getElementById("example").innerHTML = "Hint: ISBN xxx-x-xxx-xxxxx-x";
             pattern = /^ISBN\s\d{3}-\d{1}-\d{3}-\d{5}-\d{1}$/;
-            errorMessage = "Please enter a valid ISBN number.";
+            errorMessage = userInput + " is not in the correct format. Check Hint!";
             break;
 
         case "5": // Jokes
             document.getElementById("example").innerHTML = "Hint: Why did the ... ? Because...";
             pattern = /(^Why did the .+\?)\s(Because .+)+/;
-            errorMessage = "Jokes can only contain letters, digits, spaces, and the characters: '.,?!-";
+            errorMessage = userInput + " is not in the correct format. Check Hint!";
             break;
 
         case "6": // TV Episode Titles
             document.getElementById("example").innerHTML = "Hint: Show Name SXXEXX: Episode Title";
             pattern = /(.+)\d{2}E\d{2}:(.+)/;
-            errorMessage = "TV episode titles can only contain letters, digits, spaces, and the characters: '.,?!-";
+            errorMessage = userInput + " is not in the correct format. Check Hint!";
             break;
         case "7": // Weirdly Formatted Dates
             document.getElementById("example").innerHTML = "Hint: dd-MMM-yyyy";
             pattern = /^\d{2}\-[A-Z]{3}\-\d{4}$/;
-            errorMessage = "Please enter a date in the format dd-MMM-yyyy.";
+            errorMessage = userInput + " is not in the correct format. Check Hint!";
             break;
 
         case "8": // HEX Color codes
             document.getElementById("example").innerHTML = "Hint: #XXXXXX";
             pattern = /^#\w{6}$/;
-            errorMessage = "Please enter a valid HEX color code.";
+            errorMessage = userInput + " is not in the correct format. Check Hint!";
             break;
 
         case "9": // IP Address
             document.getElementById("example").innerHTML = "Hint: xxx.xxx.xxx.xxx";
             pattern = /^(\d{1,3}\.){3}\d{1,3}$/;
-            errorMessage = "Please enter a valid IP address.";
+            errorMessage = userInput + " is not in the correct format. Check Hint!";
             break;
         default:
             break;
     }
 
     if (userInput.match(pattern)) {
-        var outputMessage = userInput + " is in the correct pattern of " + selectedChoice;
+        var outputMessage = userInput + " is in the correct pattern of " + pattern;
         document.getElementById("output").innerHTML = outputMessage;
     } else {
         document.getElementById("output").innerHTML = errorMessage;
     }
+
+}
+
+
+function getMatchingStrings(regex) {
+    var set_list = [
+        'Different (1971)',
+        'Holy Grail (1975)',
+        'Brian (1979)',
+        'Moly (2000) the nother',
+        '[Verse 1] beat it boy',
+        '[Verse 500] any verse',
+        '[Verse X] everything is alright',
+        '@sir_Uracyaryamye',
+        '@umuyede6t57239629',
+        'wakanda@forever',
+        '@more_life',
+        'ISBN 978-3-126-14810-0',
+        'ISBN',
+        'ISBN 276-3-006-12310-5',
+        'Why did the tree fall down ? Because of the wind',
+        '[Verse 5] any verse',
+        '[Verse X] everything is alright',
+        'Marvelous Maisel in the forest S12E12: Boardway runner',
+        'Holy Grail (1975)',
+        'Brian (1979)',
+        'Moly (2000)',
+        '21-MAR-1982',
+        '31st-APR-1982',
+        '21-JUN-1875',
+        '09/DEC/1672',
+        '#FF0000',
+        '#800000',
+        'hvjkjhqe',
+        '#000000',
+        '255.255.255.255',
+        '127.0.0.0.0.1',
+        '127.0.0.1'
+    ];
+
+    var matches = [];
+    for (var i = 0; i < set_list.length; i++) {
+        if (regex.test(set_list[i])) {
+            matches.push(set_list[i]);
+        }
+    }
+
+    return matches;
+}
+
+
+function checkFromList() {
+    var dataType = document.getElementById("pattern_choice").value;
+    var pattern;
+    if (dataType === "1"){
+        pattern = /^(.+) \((\d{4})\)$/;
+    } else if (dataType === "2"){
+        pattern = /^\[Verse \d+\] (.+)/;
+    } else if (dataType === "3"){
+        pattern = /^@\w+/;
+    } else if (dataType === "4"){
+        pattern = /^ISBN\s\d{3}-\d{1}-\d{3}-\d{5}-\d{1}$/;
+    } else if (dataType === "5"){
+        pattern = /(^Why did the .+\?)\s(Because .+)+/
+    } else if (dataType === "6") {
+        pattern = /(.+)\d{2}E\d{2}:(.+)/;
+    } else if (dataType === "7") {
+        pattern = /^\d{2}\-[A-Z]{3}\-\d{4}$/;
+    } else if (dataType === "8") {
+        pattern = /^#\w{6}$/;
+    } else if (dataType === "9") {
+        pattern = /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}/;
+    }
+    var matchingStrings = getMatchingStrings(pattern);
+    document.getElementById("list_output").innerHTML = matchingStrings.join("<br>");
 }
