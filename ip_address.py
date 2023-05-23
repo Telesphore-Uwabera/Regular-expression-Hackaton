78a47cb7a7dcc7cad18bbab9b0f942fe3ad8d94a
Ruthuwamahoro <116666242+Ruthuwamahoro@users.noreply.github.com>
2023-05-23 12:28:44 +0200

    Update ip_address.py

--- ip_address.py
+++ ip_address.py
@@ -5,7 +5,7 @@ def ip_address(ip):
     regex = '^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}'
     match_pattern = re.search(regex, ip)
     
- if match_pattern:
+    if match_pattern:
         print(f"congrats! {ip} is in the right format.")
     else:
         print(f"ooopps! {ip} is not in the right format.")
